f1cce3acb1911a32e8fef0c3bf2b711e980e2be6
Hiram Chavez <virtual.liga@gmail.com>
2021-07-30 13:42:44 -0500

    Version final de la segunda función

--- Chunk.js
+++ Chunk.js
@@ -5,28 +5,22 @@ determinado por size. */
 function chunk(array, size) {
 
     let totalElementos = array.length;
-    //let arrayDeArrays = [];
-    let arrayTemporal = [];
-    let indice;
+    let indice = 0;
     let contador = 0;
 
-    indice = array.length%size !== 0 ? Math.trunc(array.length/size)+1 : array.length/size;
-    console.log("Indice: "+indice);
-    console.log("Size: "+size);
+    indice = array.length % size !== 0 ? Math.trunc(array.length / size) + 1 : array.length / size;
 
     let arrayDeArrays = new Array(indice);
 
-    for(let i = 0; i<indice; i++){
-        arrayDeArrays[i] = new Array(size);
+    for (let i = 0; i < indice; i++) {
 
-        for(let j = 0; j<size; j++){
-            if(contador<totalElementos){
+        i + 1 === indice && array.length % size !== 0 ? arrayDeArrays[i] = new Array(array.length % size) : arrayDeArrays[i] = new Array(size);
+
+        for (let j = 0; j < size; j++) {
+            if (contador < totalElementos) {
                 arrayDeArrays[i][j] = array[contador];
                 contador++;
-            }
-            else{
-                arrayDeArrays[i][j] = '';
-            }            
+            };
         }
     }
 
@@ -35,4 +29,6 @@ function chunk(array, size) {
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log('Test 1:', chunk(data, 3));
+console.log('Test 1:', chunk(data, 1)) 
+console.log('Test 2:', chunk(data, 2)) 
+console.log('Test 3:', chunk(data, 3))
